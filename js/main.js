48fbfae73949b8bba4f257f8c7f1c13ab20daf92
mo3az <mo3az.abdellatif@gmail.com>
2022-02-26 00:08:05 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,147 @@
+$(document).ready(function () {
+  /***** tooltip *****/
+  $('[data-toggle="tooltip"]').tooltip();
+
+  /***** product slider *****/
+  var slides = document.getElementsByClassName("products-slider");
+  for (var i = 0; i < slides.length; i++) {
+    var productswiper = new Swiper(
+      ".products-slider-" + i + " .swiper-container",
+      {
+        loop: true,
+        breakpoints: {
+          0: {
+            slidesPerView: 2,
+            spaceBetween: 8,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          992: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 31,
+          },
+        },
+        pagination: {
+          el: ".products-slider-" + i + " .swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".products-slider-" + i + " .swiper-btn-next",
+          prevEl: ".products-slider-" + i + " .swiper-btn-prev",
+        },
+        on: {
+          init: function (swiper) {
+            $('[data-toggle="tooltip"]').tooltip();
+          },
+        },
+      }
+    );
+  }
+
+  /***** testimonial slider *****/
+  var testimonialswiper = new Swiper(".testimonials-slider .swiper-container", {
+    // loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 23,
+      },
+      1199: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+    pagination: {
+      el: ".testimonials-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".testimonials-slider .swiper-btn-next",
+      prevEl: ".testimonials-slider .swiper-btn-prev",
+    },
+  });
+
+  /***** view more review *****/
+  $(".review").each(function () {
+    var height = $(this).prop("scrollHeight");
+    if (height > 73) {
+      $(this).addClass("trim");
+      $(this).siblings(".more-cont").show();
+    }
+  });
+  $(".more-text").click(function (e) {
+    var text = $(this).parents(".testimonial").find(".review");
+    var height = text.prop("scrollHeight");
+    $(this).toggleClass("active");
+    if ($(this).hasClass("active")) {
+      text.height(height).css("display", "block");
+    } else {
+      text.height(72);
+    }
+  });
+
+  /***** language country modal *****/
+  $("body").click(function (e) {
+    $(".options-dropdown").fadeOut(300);
+  });
+  $(".country-select").click(function (e) {
+    e.stopPropagation();
+    $(this).find(".options-dropdown").fadeToggle(300);
+  });
+  $(".options-dropdown .country").click(function (e) {
+    var innerText = $(this).html();
+    var countryName = $(this).find(".text").html();
+    $(".country-select .selected .country").html(innerText);
+    $(".country-select input").val(countryName);
+  });
+  $(".lang-select").select2({
+    minimumResultsForSearch: Infinity,
+    dropdownCssClass: "lang-dropdown",
+  });
+
+  $(".country-lang").click(function (e) {
+    $(".popup-overlay, .country-lang-popup").fadeIn();
+    $("body").addClass("overflow");
+  });
+  $(".popup-overlay, .close-btn").click(function (e) {
+    $(".popup-overlay, .country-lang-popup").fadeOut();
+    $("body").removeClass("overflow");
+  });
+
+  $(".cats>a").click(function (e) {
+    $(this).toggleClass("active").siblings("ul").slideToggle();
+  });
+
+  $(".has-sub>a").click(function () {
+    $(".has-sub>a").not(this).removeClass("active");
+    if ($(this).siblings("ul").css("display") == "none") {
+      $(this).addClass("active").siblings("ul").slideDown(500);
+    } else {
+      $(this).removeClass("active").siblings("ul").slideUp(500);
+    }
+    $(".has-sub>a").not(this).siblings("ul").slideUp(500);
+  });
+
+  $(".menu-btn").click(function () {
+    $("body").toggleClass("overflow");
+    $(".sidenav-overlay").fadeToggle();
+    $(".sidenav").toggleClass("active");
+    $(this).toggleClass("active");
+  });
+  $(".sidenav-overlay, .sidenav-close-btn").click(function () {
+    $("body").removeClass("overflow");
+    $(".sidenav-overlay").fadeOut();
+    $(".sidenav").removeClass("active");
+    $(".menu-btn").removeClass("active");
+  });
+});
