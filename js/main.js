8bdddeaf7c3865bfb3d568d0b8892f32821ed8d5
mo3az <mo3az.abdellatif@gmail.com>
2022-03-16 12:05:40 +0200

    reg

--- js/main.js
+++ js/main.js
@@ -241,6 +241,24 @@ $(document).ready(function () {
     $(".item-footer").slideToggle(300);
   });
 
+  const inputElements = [...document.querySelectorAll("input.code-input")];
+
+  inputElements.forEach((ele, index) => {
+    ele.addEventListener("input", (e) => {
+      const [first] = e.target.value;
+      e.target.value = first ?? ""; 
+      const lastInputBox = index === inputElements.length - 1;
+      const insertedContent = first !== undefined;
+      if (insertedContent && !lastInputBox) {
+        inputElements[index + 1].removeAttribute("disabled");
+        inputElements[index + 1].focus();
+        inputElements[index + 1].dispatchEvent(new Event("input"));
+      }
+      if (insertedContent && lastInputBox) {
+        document.getElementById("code-submit-btn").classList.remove("disabled");
+      }
+    });
+  });
 });
 function showPass(showPass) {
   sibling = showPass.parentElement.nextElementSibling;
